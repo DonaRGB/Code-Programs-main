0630d596b57497332f6e16a98717a09cac683f4c
DonaRGB <thongdadona@gmail.com>
2025-10-04 19:35:14 +0700

    Initial

--- Codes/Number of Shots Equals Number of Examples It Gets Based Off Of.py
+++ Codes/Number of Shots Equals Number of Examples It Gets Based Off Of.py
@@ -2,7 +2,7 @@ from transformers import GPT2LMHeadModel as GPTLMHM
 from transformers import GPT2Tokenizer as GPT2T
 tknzr = GPT2T.from_pretrained("gpt2")
 mdl = GPTLMHM.from_pretrained("gpt2")
-def get_rsp(prompt,max_length = 50):
+def get_rsp(prompt:str,max_length = 50):
     inputs = tknzr.encode(prompt,return_tensors = "pt")
     output = mdl.generate(inputs,max_length,num_return_sequences = 1)
     rsp = tknzr.decode(output[0],skip_special_tokens = True)
