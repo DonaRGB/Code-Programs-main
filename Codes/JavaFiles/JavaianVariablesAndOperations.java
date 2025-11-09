f4465b36b3f8ae9efb97a71a45cafe5f627cc5da
DonaRGB <thongdadona@gmail.com>
2025-11-09 17:34:25 +0700

    Initial commit

---
+++ Codes/JavaFiles/JavaianVariablesAndOperations.java
@@ -0,0 +1,9 @@
+public class JavaianVariablesAndOperations {
+    public static void main(String[] args) {
+        int x = 12;
+        int y = 23;
+        int z = x + y;
+        System.out.println(z);
+    }
+    
+}
