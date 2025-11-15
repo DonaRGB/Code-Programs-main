18e44b441cb37bd78c15558ca649f75751388be9
DonaRGB <thongdadona@gmail.com>
2025-11-15 18:05:41 +0700

    Initial commit

--- Codes/JavaFiles/LeapYearChecker.java
+++ Codes/JavaFiles/LeapYearChecker.java
@@ -3,9 +3,13 @@ public class LeapYearChecker {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a year : ");
-        int year = sc.nextInt();
-        boolean isLeapYear = (year % 400 == 0) ? true:(year % 100 == 0) ? false:(year % 4 == 0) ? true:false;
-        String isLeapYearAsWord = isLeapYear ? " is a leap year.":"is not a leap year.";
-        System.out.println("The year " + year + isLeapYearAsWord);
+        int yearinput = sc.nextInt();
+        String text = isYearALeapYear(yearinput);
+        System.out.println(text);
+    }
+    public static String isYearALeapYear(int year) {
+        boolean isLeapYear = (year % 400 == 0) ? true : (year % 100 == 0) ? false : (year % 4 == 0) ? true : false;
+        String isLeapYearAsWord = isLeapYear ? " is a leap year." : "is not a leap year.";
+        return "The year " + year + isLeapYearAsWord;
     }
 }
