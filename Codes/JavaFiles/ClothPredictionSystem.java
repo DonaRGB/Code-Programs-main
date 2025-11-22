cd79c7b417657c8aeedad2688feeff65f7716be1
DonaRGB <thongdadona@gmail.com>
2025-11-22 13:01:31 +0700

    Initial commit

---
+++ Codes/JavaFiles/ClothPredictionSystem.java
@@ -0,0 +1,15 @@
+class ClothPredictionSystem {
+    public static void main(String[] args) {
+        boolean sunny = true;
+        boolean hot = false;
+        if {sunny} {
+            if {hot} {
+                System.out.println("Use cap & half-sleeves.");
+            } else {
+                System.out.println("Use half sleeves.");
+            }
+        } else {
+            System.out.println("Take raincoat for safety.");
+        }
+    }
+}
