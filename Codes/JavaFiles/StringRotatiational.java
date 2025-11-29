7f566f29e5958db5931a91880c9769abf0b41313
DonaRGB <thongdadona@gmail.com>
2025-11-29 19:15:46 +0700

    Initial commit

--- Codes/JavaFiles/StringRotatiational.java
+++ Codes/JavaFiles/StringRotatiational.java
@@ -1,17 +1,28 @@
 import java.util.Scanner;
 import java.math.BigInteger;
+
 public class StringRotatiational {
-    public static BigInteger rotate(BigInteger n, int k, String dir) {
-        String sn = String.valueOf(n);
-        int len = sn.length();
-        k = k % len;
-        BigInteger rtdn;
-        if (dir.equalsIgnoreCase("left")) {
-            rtdn = new BigInteger(sn.substring(k) + sn.substring(0, k));
+    public static BigInteger rotate(BigInteger n, int k, boolean dir, int digits) {
+        BigInteger sign = n.signum() < 0 ? BigInteger.valueOf(-1) : BigInteger.ONE;
+        n = n.abs();
+        BigInteger powDigits = BigInteger.TEN.pow(digits);
+        n = n.mod(powDigits);
+        k = k % digits;
+        if (k == 0)
+            return n.multiply(sign);
+        BigInteger lp, rp;
+        if (dir) {
+            BigInteger move = BigInteger.TEN.pow(digits - k);
+            lp = n.mod(move);
+            rp = n.divide(move);
+            n = lp.multiply(BigInteger.TEN.pow(k)).add(rp);
         } else {
-            rtdn = new BigInteger(sn.substring(len - k) + sn.substring(0, len - k));
+            BigInteger move = BigInteger.TEN.pow(k);
+            lp = n.divide(move);
+            rp = n.mod(move);
+            n = rp.multiply(BigInteger.TEN.pow(digits - k)).add(lp);
         }
-        return rtdn;
+        return n.multiply(sign);
     }
 
     public static void main(String[] args) {
@@ -23,12 +34,15 @@ public class StringRotatiational {
         int r = scn.nextInt();
         System.out.println("Enter the direction in which to rotate (left/right):");
         d = scn.next();
+        boolean db;
         if (d.equalsIgnoreCase("l") || d.equalsIgnoreCase("left")) {
-            d = "left";
+            db = true;
         } else {
-            d = "right";
+            db = false;
         }
-        BigInteger rtd = rotate(num, r, d);
+        System.out.println("Enter the number of digits to keep :");
+        int dtokeep = scn.nextInt();
+        BigInteger rtd = rotate(num, r, db, dtokeep);
         System.out.println("The rotated form of " + num + " is " + rtd + ".");
         scn.close();
     }
