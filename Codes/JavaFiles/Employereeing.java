bcbc75ec9158be8adc77e71e2ab74cb56aee331f
DonaRGB <thongdadona@gmail.com>
2025-11-30 12:47:10 +0700

    Initial commit

---
+++ Codes/JavaFiles/Employereeing.java
@@ -0,0 +1,24 @@
+class Employee {
+    int empno;
+    String name;
+    float sal;
+    Employee() {
+        System.out.println("*****");
+        empno = 808;
+        name = "Bob";
+        sal = 5000f;
+    }
+    void displayDetails() {
+        System.out.println(empno + "|" + name + "|" + sal);
+    }
+}
+public class Employereeing {
+    public static void main(String[] args) {
+        Employee emp1 = new Employee();
+        Employee emp2 = new Employee();
+        Employee emp3 = new Employee();
+        emp1.displayDetails();
+        emp2.displayDetails();
+        emp3.displayDetails();
+    }
+}
