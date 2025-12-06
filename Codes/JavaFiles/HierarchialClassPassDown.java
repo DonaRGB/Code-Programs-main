76f023dec3d531548669a31811227c48735b33c9
DonaRGB <thongdadona@gmail.com>
2025-12-06 17:05:44 +0700

    Initial commit

---
+++ Codes/JavaFiles/HierarchialClassPassDown.java
@@ -0,0 +1,19 @@
+class Parent {
+    int age, id;
+    String name;
+    void naming(String name) {
+        System.out.println("Name : " + name);
+    }
+}
+class Child extends Parent {
+    void ageN(int age) {
+        System.out.println("Age of student is " + age);
+    }
+}
+public class HierarchialClassPassDown {
+    public static void main(String[] args) {
+        Child s = new Child();
+        s.naming("Bob");
+        s.age(12);
+    }
+}
---
+++ Codes/JavaFiles/MammalBranchingSystemForClasses.java
@@ -0,0 +1,20 @@
+class Mammals {
+    void mam() {
+        System.out.println("Inside Mammal Class");
+    }
+}
+class Lion extends Mammals {
+    void roar() {
+        System.out.println("Inside Lion Class");
+    }
+}
+class Human extends Mammals {
+    void hum() {
+        System.out.println("Inside Human Class");
+    }
+}
+public class MammalBranchingSystemForClasses {
+    public static void main(String[] args) {
+        
+    }
+}
