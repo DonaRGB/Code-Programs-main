2cfdbbb4c52b71b836d40aa680bfcea614a41131
DonaRGB <thongdadona@gmail.com>
2025-12-20 13:41:53 +0700

    Initial commit

---
+++ Codes/JavaFiles/AnAbstractObjectIsUnknownToAClass.java
@@ -0,0 +1,28 @@
+abstract class Objects {
+    abstract void showShape();
+    public void shape() {
+        System.out.println("I\'m from abstract class.");
+    }
+}
+class Sphere extends Objects {
+    void showShape() {
+        System.out.println("Object type is Sphere.");
+    }
+}
+class Cuboid extends Objects {
+    void showShape() {
+        System.out.println("Object type is Cuboid.");
+    }
+}
+class Prism extends Objects {
+    void showShape() {
+        System.out.println("Object type is Prism");
+    }
+}
+public class AnAbstractObjectIsUnknownToAClass {
+    public static void main(String[] args) {
+        Objects obj = new Sphere();
+        obj.showShape();
+        obj = new Cuboid();
+    }
+}
---
+++ Codes/JavaFiles/PolymorphisingOfNamingFamousThingsFamousPeopleDid.java
@@ -0,0 +1,48 @@
+class Hillstations {
+    void location() {
+        System.out.println("Location is :");
+    }
+    void famousfor() {
+        System.out.println("Famous for :");
+    }
+}
+class Manali extends Hillstations {
+    void location() {
+        System.out.println("Manali is in Himachal Pradesh.");
+    }
+    void famousfor() {
+        System.out.println("It is famous for Hadimba Temple and adventure sports.");
+    }
+}
+class Mussoorie extends Hillstations {
+    void location() {
+        System.out.println("Mussoorie is in Uttarakhand.");
+    }
+    void famousfor() {
+        System.out.println("It is famous for educational institutions.");
+    }
+}
+class Gulmarg extends Hillstations {
+    void location() {
+        System.out.println("Gulmarg is in Jammu.");
+    }
+    void famousfor() {
+        System.out.println("It is famous for skiing.");
+    }
+}
+public class PolymorphisingOfNamingFamousThingsFamousPeopleDid {
+    public static void main(String[] args) {
+        Hillstations A = new Hillstations();
+        Hillstations M = new Manali();
+        Hillstations Mu = new Mussoorie();
+        Hillstations G = new Gulmarg();
+        A.location();
+        A.famousfor();
+        M.location();
+        M.famousfor();
+        Mu.location();
+        Mu.famousfor();
+        G.location();
+        G.famousfor();
+    }
+}
