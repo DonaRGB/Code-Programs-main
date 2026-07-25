fdfa42b0643facbd521a2a8c2eac9c5cba837b4f
DonaRGB <thongdadona@gmail.com>
2026-07-25 17:58:44 +0700

    Initial commit

--- Codes/PythonFiles/IntersectionOfArrsOfSorts.py
+++ Codes/PythonFiles/IntersectionOfArrsOfSorts.py
@@ -1,25 +1,20 @@
-def printarray(a):
-    for n in a:
-        print(n, end = " ")
 def arrIntersection(a,b):
     m,n = len(a),len(b)
     i,j = 0,0
-    intersection = []
+    print("Intersection of a and b :")
     while i < m and j < n:
         if a[i] < b[j]:
             i += 1
         elif b[j] < a[i]:
             j += 1
         else:
-            intersection.append(b[j])
+            print("- ",b[j])
             i += 1
             j += 1
-    if intersection == []:
-        print("No intersection found")
-    else:
-        print("Intersection :", printarray(intersection))
 from random import randint as ri
-x = [ri(1,10) for _ in range(5)].sort()
-y = [ri(1,10) for _ in range(5)].sort()
+x = [ri(1,10) for _ in range(15)]
+y = [ri(1,10) for _ in range(15)]
+x.sort()
+y.sort()
 print("Original arrays :\n - a :",x,"\n - b :",y,"\n")
-print("Intersection of a and b :",arrIntersection(x,y))
+arrIntersection(x,y)
